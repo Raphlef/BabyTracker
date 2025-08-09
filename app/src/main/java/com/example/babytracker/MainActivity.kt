0fb3fcc7cd39db6c2b4a65e00821df5ae2675b56
Raphael LEFEVRE <raphio63@hotmail.fr>
2025-08-09 14:28:07 +0200

    rename old name

--- app/src/main/java/com/example/babytracker/MainActivity.kt
+++ app/src/main/java/com/example/babytracker/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.babytracker.ui.theme.BabyTrackerTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity1 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
