20255ccc7de9bae5681dd4a717b3c9ae6d58a0c0
Raphael LEFEVRE <raphio63@hotmail.fr>
2025-09-13 13:20:05 +0200

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
-    id("com.google.dagger.hilt.android") version "2.57" apply false
+    id("com.google.dagger.hilt.android") version "2.57.1" apply false
 }
 buildscript {
     repositories {
@@ -12,8 +12,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.12.3")
-        classpath("com.google.gms:google-services:4.4.3")
+        classpath(libs.gradle)
+        classpath(libs.google.services)
         // Autres classpath nécessaires
     }
 }
