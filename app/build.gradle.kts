0ea7b7e9095888a86d7faae6f56b090b11797583
Raphael LEFEVRE <raphio63@hotmail.fr>
2025-09-28 15:49:47 +0200

    Update launcher icon and splash screen
    This commit updates the application's launcher icon and splash screen.
    
    - Replaced the vector drawable for `ic_launcher_foreground.xml` with a bitmap referencing `@drawable/icontransparent`.
    - Updated `ic_launcher_round.xml` and `ic_launcher.xml` in `mipmap-anydpi-v26` to use `@color/ic_launcher_background` for the background and `@mipmap/ic_launcher_foreground` for the foreground.
    - Added a new color resource `ic_launcher_background.xml` with the value `#EBE2D3`.
    - Modified `themes.xml` to set `android:windowSplashScreenBackground` to `@drawable/background`.
    - Replaced the vector drawable for `ic_launcher_background.xml` with a bitmap referencing `@drawable/background`.
    - Updated launcher icon assets (`ic_launcher.webp`, `ic_launcher_round.webp`) across different DPIs (mdpi, hdpi, xhdpi, xxhdpi, xxxhdpi).
    - Added new foreground launcher icon assets (`ic_launcher_foreground.webp`) across different DPIs.
    - Added a Play Store icon `ic_launcher-playstore.png`.
    - Renamed `icon_transparent.png` to `icontransparent.png` in the drawable resources.
    - Increased `minSdk` from 26 to 31 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         applicationId = "com.kouloundissa.twinstracker"
-        minSdk = 26
+        minSdk = 31
         targetSdk = 36
         versionCode = getGitCommitCount()
         versionName = getGitVersionName()
