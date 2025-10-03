e9eb0c44ac8c9dc60e480f43e1989e946d92d8f7
Raphifou <lefevre@almacam.com>
2025-10-03 16:48:39 +0200

    Style: Update MonthHeader component colors
    This commit updates the color scheme of the `MonthHeader` composable for better visual consistency.
    
    - New color values `contentColor`, `backgroundColor`, and `tint` are defined using the application's theme (`BackgroundColor`, `DarkBlue`, `DarkGrey`).
    - The `IconButton` background color is updated to use the new `backgroundColor`.
    - The `Text` color for the month and year is now explicitly set to `BackgroundColor` from the theme.

--- app/src/main/java/com/kouloundissa/twinstracker/ui/components/MonthHeader.kt
+++ app/src/main/java/com/kouloundissa/twinstracker/ui/components/MonthHeader.kt
@@ -16,6 +16,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.kouloundissa.twinstracker.ui.theme.BackgroundColor
+import com.kouloundissa.twinstracker.ui.theme.DarkBlue
+import com.kouloundissa.twinstracker.ui.theme.DarkGrey
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -24,6 +27,9 @@ fun MonthHeader(
     currentMonth: LocalDate,
     onMonthChange: (deltaMonths: Long) -> Unit
 ) {
+    val contentColor = DarkGrey.copy(alpha = 0.5f)
+    val backgroundColor = BackgroundColor.copy(alpha = 0.2f)
+    val tint = DarkBlue
     Row(
         Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -31,17 +37,18 @@ fun MonthHeader(
     ) {
         IconButton(
             onClick = { onMonthChange(-1L) },
-            colors = IconButtonDefaults.iconButtonColors(containerColor = Color.LightGray.copy(alpha = 0.3f))
+            colors = IconButtonDefaults.iconButtonColors(containerColor = backgroundColor)
         ) {
             Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Previous Month")
         }
         Text(
             text = currentMonth.format(DateTimeFormatter.ofPattern("MMMM yyyy")),
-            style = MaterialTheme.typography.titleMedium
+            style = MaterialTheme.typography.titleMedium,
+            color = BackgroundColor,
         )
         IconButton(
             onClick = { onMonthChange(1L) },
-            colors = IconButtonDefaults.iconButtonColors(containerColor = Color.LightGray.copy(alpha = 0.3f))
+            colors = IconButtonDefaults.iconButtonColors(containerColor = backgroundColor)
         ) {
             Icon(Icons.AutoMirrored.Filled.ArrowForward, contentDescription = "Next Month")
         }
