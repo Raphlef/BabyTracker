daf5d6fb936973c8d8f8b89cd1c3dbc77d8170d8
Raphael LEFEVRE <raphio63@hotmail.fr>
2025-11-16 15:24:41 +0100

    style: Adjust label padding in `ModernDateSelector`
    This commit adds horizontal padding to the label within the `ModernDateSelector` component to improve its alignment and visual spacing.
    
    - **`ModernDateSelector.kt`**:
      - Added `padding(start = 4.dp)` to the `Text` composable that displays the component's label.

--- app/src/main/java/com/kouloundissa/twinstracker/ui/components/ModernDateSelector.kt
+++ app/src/main/java/com/kouloundissa/twinstracker/ui/components/ModernDateSelector.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.AlertDialog
@@ -36,9 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.kouloundissa.twinstracker.ui.theme.BackgroundColor
@@ -83,7 +80,13 @@ fun ModernDateSelector(
         modifier = modifier,
     ) {
         Column(modifier = Modifier.padding(12.dp)) {
-            Text(label, style = MaterialTheme.typography.labelMedium, color = contentcolor)
+            Text(
+                label,
+                style = MaterialTheme.typography.labelMedium,
+                color = contentcolor,
+                modifier = Modifier
+                    .padding(start = 4.dp)
+            )
             Spacer(Modifier.height(6.dp))
             Row(
                 verticalAlignment = Alignment.CenterVertically,
